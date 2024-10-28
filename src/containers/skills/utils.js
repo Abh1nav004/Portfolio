da34f782fb8086a724e04d16ee41e460a917849f
Abh1nav004 <abhinavsharma00410@gmail.com>
2024-10-28 17:08:32 +0530

    Skills Page Completed with scss file and animations

---
+++ src/containers/skills/utils.js
@@ -0,0 +1,69 @@
+export const skillsData = [
+    {
+        label: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: "90",
+            },
+            {
+                skillName: "CSS",
+                percentage: "50",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage: "90",
+            },
+            {
+                skillName: "REACT JS",
+                percentage: "90",
+            },
+            {
+                skillName: "TYPESCRIPT",
+                percentage: "90",
+            },
+            {
+                skillName: "TAILWIND",
+                percentage: "90",
+            },
+
+        ],
+    },
+    {
+        label: "BACK END",
+        data: [
+            {
+                skillName: "JAVA",
+                percentage: "90",
+            },
+            {
+                skillName: "PYTHON",
+                percentage: "90",
+            },
+            {
+                skillName: "C#",
+                percentage: "90",
+            },
+        ]
+    },
+    {
+        label: "DATABASES",
+        data: [
+            {
+                skillName: "MYSQL",
+                percentage: "90",
+            },
+            {
+                skillName: "MONGODB",
+                percentage: "90",
+            },
+            {
+                skillName: "ORACLE",
+                percentage: "90",
+            },
+        ]
+    }
+
+
+]
+
