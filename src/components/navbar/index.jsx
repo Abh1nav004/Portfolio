845d9f917fb90bb25be9c021b61b9a6b3e3cd4ce
Abh1nav004 <abhinavsharma00410@gmail.com>
2024-10-30 01:24:10 +0530

    navbar fixed for mobile view

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import {FaBars ,FaReact } from "react-icons/fa";
+import { FaBars, FaReact } from "react-icons/fa";
 import { HiX } from "react-icons/hi";
 import { Link } from "react-router-dom";
-import './styles.scss'
+import './styles.scss';
+
 const data = [
   {
     label: "HOME",
@@ -30,37 +31,42 @@ const data = [
   },
 ];
 
-
 const Navbar = () => {
-    const [toggleIcon,setToggleIcon] =useState(false)
-;    const handleToggleIcon = () => {
-        setToggleIcon(!toggleIcon);
-    
+  const [toggleIcon, setToggleIcon] = useState(false);
+
+  const handleToggleIcon = () => {
+    setToggleIcon(!toggleIcon);
+  };
+
+  const handleLinkClick = () => {
+    if (toggleIcon) {
+      setToggleIcon(false); // Close the menu when a link is clicked
     }
+  };
 
   return (
     <div>
       <nav className="navbar">
         <div className="navbar__container">
-          <Link to={"/"} className="navbar__container__logo">
+          <Link to="/" className="navbar__container__logo">
             <FaReact size={30} />
           </Link>
         </div>
-        <ul className={`navbar__container__menu  ${toggleIcon?'active' : ''}`}>
+        <ul className={`navbar__container__menu ${toggleIcon ? 'active' : ''}`}>
           {data.map((item, key) => (
             <li key={key} className="navbar__container__menu__item">
               <Link
                 to={item.to}
                 className="navbar__container__menu__item__links"
-                >{item.label}
+                onClick={handleLinkClick} // Close the menu on link click
+              >
+                {item.label}
               </Link>
             </li>
           ))}
         </ul>
         <div className="nav-icon" onClick={handleToggleIcon}>
-          {
-            toggleIcon? <HiX size={30} /> : <FaBars size={30} />
-          }
+          {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
         </div>
       </nav>
     </div>
