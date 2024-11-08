c38069da97fbf47f4aaaa06823cbd640f0a2d6e8
Abh1nav004 <abhinavsharma00410@gmail.com>
2024-11-08 12:47:23 +0530

    font changes

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -28,11 +28,24 @@ const personalDetails = [
     },
 ]
 
-const jobSummary = `Hello! I'm Abhinav Sharma, a passionate and creative software developer currently pursuing my BTech in Computer Science and Engineering at Lovely Professional University. My journey in tech has been fueled by a love for problem-solving and a drive to create impactful solutions. I’ve had the opportunity to work on diverse projects, including EliteCare, a healthcare platform that secured a spot in the Smart India Hackathon, and a smart Discord Bot designed with AI enhancements for community management.
+const jobSummary = `Hello! I'm Abhinav Sharma, a passionate and creative software developer currently pursuing my BTech in Computer Science and Engineering at Lovely Professional University. My journey in tech has been fueled by a love for problem-solving and a drive to create impactful solutions.
+
+I’ve had the opportunity to work on diverse projects, including EliteCare, a healthcare platform that secured a spot in the Smart India Hackathon, and a smart Discord Bot designed with AI enhancements for community management.
 
 Alongside my technical skills in languages like Java, Python, and JavaScript, I'm proficient in full-stack development using frameworks like Node.js, React, and Angular. I am also experienced in database management with MongoDB, MySQL, and Postgres, and I enjoy exploring AI integrations to make applications more intuitive and efficient.
 
-Beyond coding, I’m an active participant in the tech community, currently serving as a Campus Ambassador for IIT Bombay, where I collaborate on national events and tech challenges. My goal is to bring creative ideas and technical depth to every project, always aiming to create solutions that make a difference.`
+Beyond coding, I’m an active participant in the tech community, currently serving as a Campus Ambassador for IIT Bombay, where I collaborate on national events and tech challenges. My goal is to bring creative ideas and technical depth to every project, always aiming to create solutions that make a difference.`;
+
+<p className="jobsum">
+  {jobSummary.split("\n").map((text, index) => (
+    <span key={index}>
+      {text}
+      <br />
+      <br />
+    </span>
+  ))}
+</p>
+
 
 const About = () => {
     return (
@@ -57,7 +70,7 @@ const About = () => {
                         }}
                     >
                         <h3>Developer</h3>
-                        <p>{jobSummary}</p>
+                        <p className="jobsum">{jobSummary}</p>
                     </Animate>
 
 
